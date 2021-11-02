57d60888e37743bd05a1b1bd4ebe48de4204b452
shubh25940000 <shubh2594@gmail.com>
2021-11-02 07:33:50 +0530

    Version 2. Hid API Keys

--- final_project/translator.py
+++ final_project/translator.py
@@ -1,12 +1,14 @@
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-url_lt='https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/2874ea28-1865-424e-bfa6-1ab723b05603'
-apikey_lt='62kSDjd2RlPmNuj8SwzWAV0wCHM_wdE92C5YkKl_pjSH'
+from dotenv import load_dotenv
+load_dotenv('keys.env')
+import os
+url_lt= os.getenv('URL')
+apikey_lt= os.getenv('API_KEY')
 version_lt='2018-05-01'
 authenticator = IAMAuthenticator(apikey_lt)
 language_translator = LanguageTranslatorV3(version=version_lt,authenticator=authenticator)
 language_translator.set_service_url(url_lt)
-text_translate = 'Hello, how are you?'
 
 def eng_to_french(text_translate):
     translation_response = language_translator.translate(text=text_translate, model_id='en-fr')
